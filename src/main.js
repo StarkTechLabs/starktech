24cd6604bfbca663e4ed7a9a8f03eac6d390e9c5
mstark5652 <mstark5652@gmail.com>
2023-05-04 18:32:07 -0500

    feat: start of agency site rebrand

--- src/main.js
+++ src/main.js
@@ -1,93 +1,34 @@
-const THREE = window.THREE
-let container, scene, camera, renderer, controls
-var objects
 
-const init = () => {
-  container = document.getElementById('container')
-  camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 15000)
-  camera.position.z = 5000
+const registerScrollControl = () => {
+  const parallax = document.querySelector('.parallax-container')
+  parallax.addEventListener('scroll', () => {
+    const ele = document.querySelector('.scroll-indicator')
+    if (ele && ele.style) {
+      ele.style.opacity = 1 - parallax.scrollTop / 75
+    }
+  })
+}
 
-  scene = new THREE.Scene()
-  // scene.background = new THREE.Color().setHSL(0.51, 0.4, 0.01)
-  scene.fog = new THREE.Fog(scene.background, 3500, 15000)
-
-  // world
-
-  const s = 250
-  const geometry = new THREE.BoxBufferGeometry(s, s, s)
-  const material = new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0xffffff, shininess: 50 })
-
-  objects = []
-
-  for (var i = 0; i < 1000; i++) {
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.x = 8000 * (2.0 * Math.random() - 1.0)
-    mesh.position.y = 8000 * (2.0 * Math.random() - 1.0)
-    mesh.position.z = 8000 * (2.0 * Math.random() - 1.0)
-
-    mesh.rotation.x = Math.random() * Math.PI
-    mesh.rotation.y = Math.random() * Math.PI
-    mesh.rotation.z = Math.random() * Math.PI
-
-    mesh.matrixAutoUpdate = false
-    mesh.updateMatrix()
-
-    objects.push(mesh)
-    scene.add(mesh)
+const registerSectionScrollFade = () => {
+  const parallax = document.querySelector('.parallax-container')
+  let positionTop = 0
+  for (const sec of parallax.children) {
+    const { positionTop: current } = Object.assign({}, { positionTop }) // copy value
+    positionTop += sec.clientHeight
+    // console.log('section scroll listener', sec.id)
+    parallax.addEventListener('scroll', () => {
+      const offsetModify = window.innerHeight > 100 ? 150 : 250
+      sec.style.opacity = 1 - (parallax.scrollTop - current + offsetModify) / (window.innerHeight * 0.1)
+    })
   }
-
-  // lights
-
-  const dirLight = new THREE.DirectionalLight(0x5588aa, 0.30)
-  dirLight.position.set(0, -1, 0).normalize()
-  dirLight.color.setRGB(85 / 255, 136 / 255, 170 / 255)
-  scene.add(dirLight)
-
-  // renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.outputEncoding = THREE.sRGBEncoding
-  container.appendChild(renderer.domElement)
-
-  // controls
-  controls = new window.MapControls(camera, renderer.domElement)
-  controls.enableDamping = true // an animation loop is required when either damping or auto-rotation are enabled
-  controls.dampingFactor = 0.05
-
-  controls.autoRotate = true
-  controls.autoRotateSpeed = 0.5
-
-  controls.screenSpacePanning = false
-
-  controls.minDistance = 10
-  controls.maxDistance = 10000
-
-  controls.maxPolarAngle = Math.PI / 2
-
-  window.addEventListener('resize', onWindowResize, false)
 }
 
-const onWindowResize = () => {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-}
+registerScrollControl()
+registerSectionScrollFade()
 
-const render = () => {
-  for (const item of objects) {
-    item.rotation.x += 0.005
-    item.rotation.y += 0.007
-    item.updateMatrix()
-  }
-  controls.update()
-  renderer.render(scene, camera)
-}
-
-const animate = () => {
-  window.requestAnimationFrame(animate)
-  render()
-}
-
-init()
-animate()
+// window.addEventListener('resize', () => {
+//   const parallax = document.querySelector('.parallax-container')
+//   parallax.removeEventListener('scroll')
+//   registerScrollControl()
+//   registerSectionScrollFade()
+// })
